c99e8f5313d26eff8e18dcf291f151a699604c91
GCIS-web-creator <580098@tokyo.soka.ed.jp>
2026-09-15 07:47:07 +0000

    Set up Vite React project

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CityGridIso from './citybuilder_iso3d_3lane_highway_part5__9_.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CityGridIso />
+  </React.StrictMode>
+);
